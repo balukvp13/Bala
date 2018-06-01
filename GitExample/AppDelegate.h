ad21bad68748ffd8097fdecc1407d8c8d3b1c14b
TKM <tkm@santhu-macbook-pro.local>
2018-06-01 11:15:43 +0530

    Initial Commit

---
+++ GitExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitExample
+//
+//  Created by TKM on 01/06/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitExample
+//
+//  Created by TKM on 01/06/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
